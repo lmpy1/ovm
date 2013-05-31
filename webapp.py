f377396a1fab3de936000aa231e95df2784cd917
Sören Wegener <wegener92@gmail.com>
2013-05-31 13:43:16 +0200

    Basic navigation.

--- webapp.py
+++ webapp.py
@@ -10,5 +10,10 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/service')
+def service():
+    return render_template('service.html')
+
+
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', debug=True)
