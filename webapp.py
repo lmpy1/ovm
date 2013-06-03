5d5231c5e4d841d69bd5c2e463729379413fe8ee
Sören Wegener <wegener92@gmail.com>
2013-06-03 12:38:42 +0200

    Added some pages.

--- webapp.py
+++ webapp.py
@@ -1,18 +1,38 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
+    return redirect(url_for('home'))
+
+
+@app.route('/home')
+def home():
     return render_template('index.html')
 
 
 @app.route('/service')
 def service():
     return render_template('service.html')
+
+
+@app.route('/products')
+def products():
+    return render_template('products.html')
+
+
+@app.route('/products/guarantee')
+def product_guarantee():
+    return render_template('product_guarantee.html')
+
+
+@app.route('/products/guarantee/demo')
+def demo_index():
+    return render_template('demo/index.html')
 
 
 if __name__ == '__main__':
